0a1b86ed6b2ccfa5eacf8fe5afb288e0a53b23a4
Arno Sonck <sonck@r-n0.com>
2021-09-08 20:58:29 -0500

    I added exceptios and assert statements to debugging.py

--- debugging.py
+++ debugging.py
@@ -2,9 +2,22 @@ def divisors(num):
     divisors = [i for i in range(1,num+1) if num % i == 0]
     return divisors
 
+# usando excepciones
+# def run():
+#     try:
+#         num = int(input("ingresa un número: "))
+#         if num <= 0:
+#             raise ValueError
+#         print(divisors(num))
+#         print("Termino mi programa")
+#     except ValueError:
+#         print("Debes ingresar un número entero positivo")
+
+# usando assert statements
 def run():
-    num = int(input("ingresa un número: "))
-    print(divisors(num))
+    num = input("ingresa un número: ")
+    assert num.isnumeric(),"Debe ingresar un número positivo"  
+    print(divisors(int(num)))
     print("Termino mi programa")
 
 if __name__=='__main__':
